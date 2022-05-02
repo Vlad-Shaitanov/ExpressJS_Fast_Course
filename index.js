3e564452b2e31612da03e2e7d1d396481f7a57e4
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2022-05-02 15:52:39 +0300

    Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path"; // Работа с путями
 import { requestTime, logger } from "./middlewares.js";// Мидлвары для расширения запросов
+import serverRoutes from "./routes/servers.js";
 
 // Определение директории
 const __dirname = path.resolve();
@@ -11,11 +12,14 @@ const PORT = process.env.PORT ?? 3000;
 //Инициализация приложения
 const app = express();
 
+/*=================================Пример работы со статичными страницами ================*/
+
 /* Делаем папку статичной. Это позволяет нам не делать get-запрос к каждой
 	странице, а работать сразу со всей папкой*/
-app.use(express.static(path.resolve(__dirname, "static")));
+// app.use(express.static(path.resolve(__dirname, "static")));
 app.use(requestTime);
 app.use(logger);
+app.use(serverRoutes);
 
 
 // обрабатываем get-запрос по указанному урлу
@@ -30,12 +34,28 @@ app.use(logger);
 // 	response.sendFile(path.resolve(__dirname, "static", "index.html"));
 // });
 
-app.get(("/download"), (request, response) => {
-	console.log(request.requestTime);
-	// Скачивание определенной страницы
-	response.download(path.resolve(__dirname, "static", "index.html"));
+// app.get(("/download"), (request, response) => {
+// 	console.log(request.requestTime);
+// 	// Скачивание определенной страницы
+// 	response.download(path.resolve(__dirname, "static", "index.html"));
+// });
+/*=================================Конец примера работы со статичными страницами ================*/
+
+
+/*==================================Пример работы с шаблонизатором EJS============================*/
+//Переменные
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "ejs"));
+// console.log(app.get("views"));
+
+app.get("/", (req, res) => {
+	res.render("index", { title: "Main page", active: "main" });
 });
 
+app.get("/features", (req, res) => {
+	res.render("features", { title: "Features page", active: "features" });
+});
+/**/
 
 //Задаем порт, на котором будем запускать
 app.listen(PORT, () => {
